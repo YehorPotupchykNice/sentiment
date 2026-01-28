62c86845f87b8814961b98693960ebcaac26912f
Yehor Potupchyk <yehor.potupchyk@nice.com>
2026-01-28 10:00:31 +0100

    add simple cases

--- src/main/java/org/example/sentiment/TextAnalyzer.java
+++ src/main/java/org/example/sentiment/TextAnalyzer.java
@@ -1,7 +1,9 @@
 package org.example.sentiment;
 
 public class TextAnalyzer {
+    //description of the task is very vague - so let's do some complete bullshit here
+    //score will be simply calculated by ratio of 3 words - shit, mid and fire
     public static SentimentScore sentimentScore(String text) {
-
+        return new SentimentScore(0, 0, 0);
     }
 }
--- src/test/java/org/example/sentiment/TextAnalyzerTests.java
+++ src/test/java/org/example/sentiment/TextAnalyzerTests.java
@@ -1,13 +1,32 @@
 package org.example.sentiment;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.Arrays;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 public class TextAnalyzerTests {
-    @Test
-    public void testSentimentScore() {
 
+    static Stream<Arguments> testSentimentScoreProvider() {
+        return Stream.of(
+                arguments("shit", new SentimentScore(1, 0, 0)),
+                arguments("mid", new SentimentScore(0, 1, 0)),
+                arguments("fire", new SentimentScore(0, 0, 1))
+
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("testSentimentScoreProvider")
+    public void testSentimentScore(String text, SentimentScore expectedResult) {
+        assertEquals(expectedResult, TextAnalyzer.sentimentScore(text));
     }
 }
