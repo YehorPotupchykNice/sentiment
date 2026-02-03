bcbfe3399deb5ca722cab8f03e35cd35d6653058
Yehor Potupchyk <yehor.potupchyk@nice.com>
2026-02-03 09:32:05 +0100

    test null input

--- src/test/java/org/example/sentiment/service/SegmentAnalysisServiceImplTests.java
+++ src/test/java/org/example/sentiment/service/SegmentAnalysisServiceImplTests.java
@@ -1,4 +1,18 @@
 package org.example.sentiment.service;
 
+import org.junit.jupiter.api.Test;
+import tools.jackson.databind.ObjectMapper;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
 public class SegmentAnalysisServiceImplTests {
+
+    @Test
+    public void testNull() {
+        var segmentAnalysisService = new SegmentAnalysisServiceImpl();
+        var result = segmentAnalysisService.analyze(null);
+        assertNull(result);
+    }
 }
